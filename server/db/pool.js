d96fb99cd710710fc83acbad4c5b0efdd2027fe6
garvthakur123 <garvthakur123@gmail.com>
2026-03-21 18:44:41 +0100

    Inital BE and DB setup

---
+++ server/db/pool.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql2/promise");
+
+module.exports = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: 10,
+});
